179c4ceba2e28455676c1e277a739dd5286610e9
zeineb <zeinebm24@gmail.com>
2024-10-29 19:23:38 +0100

    ajout des fichiers

---
+++ script.js
@@ -0,0 +1,18 @@
+    // // toggle menu/navbar script
+    // $('.menu-btn').click(function(){
+    //     $(' header  .navigation').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Data Analyst","Business Analyst","Web Developper"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Data Analyst", "Business Analyst","Web Developper"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
